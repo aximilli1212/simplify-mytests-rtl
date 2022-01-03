8ceab26a065a861ca22bfbd59eff4d2345f086a7
Emmanuel Sackey <aximilli1212@gmail.com>
2022-01-03 18:15:01 +0000

    use screen debug() method to inspect The test DOM

--- src/home/Travel.spec.js
+++ src/home/Travel.spec.js
@@ -4,18 +4,31 @@ import Travel from "./Travel";
 describe('Travel Element', ()=>{
     it('displays the header and paragraph text',  ()=>{
         const {container} = render(<Travel/>) // container represents the resulting HTML output of the component
+         // screen.debug()
+
         expect(container).toMatchInlineSnapshot(
             `
-            <div>
-            <div class="card text-center m-1"
-                 style="width: 18rem"
-            >
-            <i class="material-icons"
-               style="font-size: 4rem"
-             > 
-                airplanemode_active
-            </i>
-            </div>
+             <div>
+                <div
+                  class="card text-center m-1"
+                  style="width: 18rem;"
+                >
+                  <i
+                    class="material-icons"
+                    style="font-size: 4rem;"
+                  >
+                    airplanemode_active
+                  </i>
+                  <h4>
+                    Travel Anywhere
+                  </h4>
+                  <p
+                    class="p-1"
+                  >
+                    Our premium package allows you to take exotic trips anywhere at the cheapest prices!
+                  </p>
+                </div>
+              </div>
             `
         )
     })
