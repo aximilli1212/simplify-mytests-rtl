6436b8825ba1bf78b240e76410e711cce8381d23
Emmanuel Sackey <aximilli1212@gmail.com>
2022-01-03 20:01:40 +0000

    Add Votes to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './styles/App.css';
 // import Jumbotron from "./home/Jumbotron";
 import Table from "./home/Table";
+import Vote from "./pageActions/vote";
 
 function App() {
   return (
     <div className="App">
+      <Vote totalGlobalLikes={4} />
       {/*<Jumbotron />*/}
       {/*  <Table employees={[{id: 1, name:"alice", department: "Moon", title: "off"}]} />*/}
     </div>
