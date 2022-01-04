4c92fc2c64500a43dca49941af2dece9a4b47c45
Emmanuel Sackey <aximilli1212@gmail.com>
2022-01-04 15:19:47 +0000

    Add extra mocks and install msw

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './styles/App.css';
 import Table from "./home/Table";
 import Vote from "./pageActions/vote";
 import EmployeeEmail from "./pageActions/EmployeeEmail";
+import DrinkSearch from "./pageActions/DrinkSearch";
 
 function App() {
   return (
     <div className="App">
       <Vote totalGlobalLikes={4} />
         <EmployeeEmail />
+        <DrinkSearch />
       {/*<Jumbotron />*/}
       {/*  <Table employees={[{id: 1, name:"alice", department: "Moon", title: "off"}]} />*/}
     </div>
